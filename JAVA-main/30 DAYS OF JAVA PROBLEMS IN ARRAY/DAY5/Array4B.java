62887469e9be33b734dc36b21843bc4252229ebf
Valiwri <jecynvallirieturbanos@gmail.com>
2026-02-15 11:51:16 +0800

    This is for day 5

---
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY5/Array4B.java
@@ -0,0 +1,7 @@
+package DAY5;
+
+public class Array4B {
+    public static void main(String[] args) {
+        int [] arr = {2, 4, 4, 5, 6, 6, 9, 20, 10};
+    }
+}
--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY5/Array5.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY5/Array5.java
@@ -1,16 +1,17 @@
 package DAY5;
-import java.util.Scanner;
 public class Array5 {
     public static void main(String[] args) {
-        int [] arr = {1, 2, 5, 4, 8, 7, 11, 10};
-        Scanner cs = new Scanner(System.in);
-
+        int [] arr = {1, 3, 5, 2, 5, 4 ,8, 8, 7, 7, 11, 10};
+        
         for(int s : arr){
             System.out.print(s + " ");
         }
 
         int added = 0;
         int adder = 1;
+
+        int added1 = 0;
+        int adder2 = 1;
 
         for(int i = 0; i < arr.length-1;i++){
             if(arr[added] > arr[adder]){
@@ -22,11 +23,33 @@ public class Array5 {
             adder++;
         }
 
-        System.out.println("\n");
+        for(int i = 0; i < arr.length-1;i++){
+            if(arr[added1] > arr[adder2]){
+                int temp = arr[added1];
+                arr[added1] = arr[adder2];
+                arr[adder2] = temp;
+            }
+            added1++;
+            adder2++;
+        }
+
+        System.out.println("\nSorted Array");
         for(int m : arr){
             System.out.print(m + " ");
         }
 
+        int index = 1;
+
+        for(int i = 1; i < arr.length;i++){
+            if(arr[i] != arr[i - 1]){
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        for(int i =0; i < index;i++){
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
