74b8bbfae0acd7bb0e7422f35cee1b0b49ae0ec0
Valiwri <jecynvallirieturbanos@gmail.com>
2026-02-15 12:29:56 +0800

    There are fresh

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY6/Array6.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY6/Array6.java
@@ -1,5 +1,19 @@
 package DAY6;
 
-public class Array6 {
-    
+public class Array6{
+	public static void main(String [] args){
+	int [] arr{21, 20, 31, 56, 30, 40, 10, 4};
+	
+	int end = arr.length - 1;
+
+	for(int i = 0; i < arr.length;i++){
+	if(arr[i] % 10 == 0){
+	int temp = arr[i];
+	arr[i] = arr[end];
+	arr[end] = temp;
+
+	end--;
+	}
+	}
+}
 }
