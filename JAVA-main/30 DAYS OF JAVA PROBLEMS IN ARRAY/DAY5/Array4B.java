185332748f5f26c92f2491d51b36a7eb90c653e6
Valiwri <jecynvallirieturbanos@gmail.com>
2026-02-15 12:05:58 +0800

    This is newly fresh code

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY5/Array4B.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY5/Array4B.java
@@ -1,7 +1,10 @@
 package DAY5;
-
+import java.util.Scanner;
 public class Array4B {
     public static void main(String[] args) {
         int [] arr = {2, 4, 4, 5, 6, 6, 9, 20, 10};
+        Scanner cs = new Scanner(System.in);
+
+        
     }
 }
