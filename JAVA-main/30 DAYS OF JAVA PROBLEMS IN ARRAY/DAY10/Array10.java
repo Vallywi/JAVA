6468fba189471293fe272e1d7185447784a5da1f
Valiwri <jecynvallirieturbanos@gmail.com>
2026-02-27 13:36:51 -1100

    This is new

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY10/Array10.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY10/Array10.java
@@ -9,17 +9,16 @@ package DAY10;
 // 5 => 1
 // 6 => 1
 // 7 => 2
+// 9 -> 3
 
 import java.util.*;
 class Solution{
-    public HashMap<Integer, List<Integer>> frequent(int [] nums){
+    public void frequent(int [] nums){
         HashMap <Integer, List<Integer>> map = new HashMap<>();
         
         for(int i = 0; i < nums.length;i++){
-            map.putIfAbsent(nums[i], new ArrayList<>());
-            map.get(nums[i]).add((1)+1);
+            map.put(nums[i], map.getOrDefault(map,0 null)+1);
         }
-        return map;
     }
 }
 
