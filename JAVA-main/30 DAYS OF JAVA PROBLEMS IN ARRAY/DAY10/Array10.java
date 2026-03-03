1a3ad9a6f751934a660975b31c5cf8c67f750dd1
Valiwri <jecynvallirieturbanos@gmail.com>
2026-03-02 19:29:52 -1200

    Count frequency of each element

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY10/Array10.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY10/Array10.java
@@ -13,24 +13,33 @@ package DAY10;
 
 import java.util.*;
 class Solution{
-    public void frequent(int [] nums){
-        HashMap <Integer, List<Integer>> map = new HashMap<>();
+    public HashMap <Integer, Integer> counter (int [] nums){
+        HashMap<Integer, Integer> map1 = new HashMap<>();
         
         for(int i = 0; i < nums.length;i++){
-            map.put(nums[i], map.getOrDefault(map,0 null)+1);
+            if(map1.containsKey(nums[i])){
+                int currentcount = map1.get(nums[i]);
+                map1.put(nums[i], currentcount + 1);
+            }else{
+                map1.put(nums[i], 1);
+            }
         }
+        return map1;
     }
+    
 }
 
 
-public class Array10 {
-    public static void main(String[] args) {
-        int [] nums = {2, 2, 1, 3, 4, 5, 6, 7, 7};
+public class Array10{
+    public static void main(String [] args){
+        int [] nums = {2, 3, 5, 10, 6, 1, 1, 1, 2, 4, 10};
         Solution sol = new Solution();
-        HashMap<Integer, List<Integer>> result = sol.frequent(nums);
-
-        for(int answer : result.keySet()){
-            System.out.println(answer + " => " + result.get(answer));
+        
+        HashMap <Integer, Integer> result = sol.counter(nums);
+        
+        for(int s: result.keySet()){
+            System.out.println(result.get(s) + " => " + s);
         }
+        
     }
 }
