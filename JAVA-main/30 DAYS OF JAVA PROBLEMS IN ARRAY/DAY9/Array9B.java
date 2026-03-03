6716fe32a8c410cfc540c96863868ca3a72d526b
Valiwri <jecynvallirieturbanos@gmail.com>
2026-03-02 14:34:09 -1200

    This is using optimization

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY9/Array9B.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY9/Array9B.java
@@ -1,38 +1,27 @@
-//Find all pairs that sum to a target (optimized)
-// using the hashmaps
+//this is with the use of optimization
 
-//[4, 2, 3, 5, 7, 9, 10, 14, 20]
-//target = 9;
+
+package DAY9;
 
 import java.util.*;
-class Solution{
-        public static List<int[]> twoSum(int[] nums, int target) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        List<int[]> pairs = new ArrayList<>();
-    
-
-        for(int i = 0; i < nums.length;i++){
-            int need = target - nums[i];
-            
-            if(map.containsKey(need)){
-                pairs.add(new int [] {need, nums[i]});
-            }
-            map.put(nums[i], i);
-        }
-        return pairs;
-    }
-}
-
-
-class Array9B{
+public class Array9B{
     public static void main(String [] args){
-        int [] nums = {4, 2, 3, 5, 7, 9, 10, 14, 20};
+        HashMap<Integer, Integer> map1 = new HashMap<>();
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+        int [] nums = {7, 1, 2, 5, 4, 9, 10, 11, 11};
         int target = 9;
-        Solution sol = new Solution();
-        List <int[]> answer = sol.twoSum(nums, target);
-        for(int[] pair : answer){
-            System.out.println(pair[0] + " + " + pair[1]);
+        
+        for(int i = 0; i < nums.length;i++){
+            if(map1.containsKey(target-nums[i])){
+                map2.put(nums[i], map1.get(target-nums[i]));
+            }
+            map1.put(nums[i], i);
         }
+        
+        for(int s : map2.keySet()){
+            System.out.println(map2.get(s) + " " + s);
+        }
+        
         
     }
 }
