f802485669b8eb18b5bd5ebc4f293d6a503421bc
Valiwri <jecynvallirieturbanos@gmail.com>
2026-03-02 19:49:16 -1200

    Check if two arrays are equal by value in order

---
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY10/Array10B.java
@@ -0,0 +1,34 @@
+package DAY10;
+import java.util.*;
+
+class Solution{
+    public boolean sorter(int [] nums1, int [] nums2){
+        if(nums1.length != nums2.length){
+            return false;
+        }
+        
+        for(int i = 0; i < nums1.length; i++){
+            if(nums1[i] != nums2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
+
+public class Array10B{
+    public static void main(String [] args){
+        int [] nums1 = {2, 3, 1, 1, 4, 5, 6};
+        int [] nums2 = {2, 3, 1, 1, 5, 4, 6};
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        Solution sol = new Solution();
+        boolean result = sol.sorter(nums1, nums2);
+        
+        System.out.println(result);
+        
+    }
+}
