3df38db0a03d27887823810748e4df49a89a07c5
Valiwri <jecynvallirieturbanos@gmail.com>
2026-03-02 14:46:40 -1200

    This is using optimization

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY9/Array9B.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY9/Array9B.java
@@ -1,27 +1,33 @@
-//this is with the use of optimization
-
-
-package DAY9;
+// INT [] NUMS = [7, 1, 2, 5, 4, 9, 10, 11, 11]
+//OUTPUT SHOULD BE THE INDEXES
 
 import java.util.*;
-public class Array9B{
-    public static void main(String [] args){
+
+class Solution{
+    public HashMap <Integer, Integer> Targeter(int [] nums, int target){
         HashMap<Integer, Integer> map1 = new HashMap<>();
         HashMap<Integer, Integer> map2 = new HashMap<>();
-        int [] nums = {7, 1, 2, 5, 4, 9, 10, 11, 11};
-        int target = 9;
-        
         for(int i = 0; i < nums.length;i++){
             if(map1.containsKey(target-nums[i])){
                 map2.put(nums[i], map1.get(target-nums[i]));
             }
             map1.put(nums[i], i);
         }
+        return map2;
         
-        for(int s : map2.keySet()){
-            System.out.println(map2.get(s) + " " + s);
+    }
+}
+public class Array9B{
+    public static void main(String [] args){
+        int [] nums = {7, 1, 2, 5, 4, 9, 10, 11, 11};
+        int target = 9;
+        
+        Solution sol = new Solution();
+        HashMap <Integer, Integer> result = sol.Targeter(nums, target);
+        
+        for(int s : result.keySet()){
+            System.out.println(result.get(s) + " " + s);
         }
-        
         
     }
 }
