d0ea10c8b102e6b7d2d44631434cd6cd8ff11ba8
Vallirie <jecynvallirieturbanos@gmail.com>
2026-03-07 08:20:26 +0800

    Rename class from Array11BA to Array11BB
    find the duplicate nums (Hashing)

---
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY11/Array11BB.java
@@ -0,0 +1,33 @@
+// find the duplicate nums (Hashing)
+// nums = [1, 2, 2, 3, 4, 4, 5, 5, 6, 7, 8,10, 10, 9]
+// expected output : 2 4 5 10
+import java.util.*;
+class Solution{
+    HashSet <Integer> duplicas (int [] nums){
+        HashSet <Integer> yes = new HashSet<>();
+        HashSet <Integer> no = new HashSet <>();
+        
+        for(int i = 0; i < nums.length;i++){
+            if(yes.contains(nums[i])){
+                no.add(nums[i]);
+            }
+            yes.add(nums[i]);
+        }
+        return no;
+         
+    }
+    
+}
+public class Array11BB{
+    public static void main(String [] args){
+        int [] nums = {1, 2, 2, 3, 4, 4, 5, 5, 6, 7, 8,10, 10, 9};
+        
+        Solution sol = new Solution();
+        HashSet <Integer> result = sol.duplicas(nums);
+        
+        for(int s : result){
+            System.out.print(s + " ");
+        }
+        
+    }
+}
