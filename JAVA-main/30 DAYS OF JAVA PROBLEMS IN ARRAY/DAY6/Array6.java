87cc378d3cbc8aadceb8cbe7886aaf2d493f3899
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-03 22:38:26 -1200

    Move all zeros to end of array

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY6/Array6.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY6/Array6.java
@@ -1,35 +1,22 @@
 package DAY6;
-
-// [21, 20, 31, 56, 30, 40, 10, 4]
-// [21, 4, 31, 56, 40, 10, 30, 20]
-
-//so mali pala thiz
+//Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+//Output: [1, 2, 4, 3, 5, 0, 0, 0]
 
 class Array6{
-	public static void main(String [] args){
-	int [] arr = {21, 20, 31, 56, 30, 40, 10, 4};
-	
-		System.out.println("Original Array:");
-	for(int f : arr){
-
-		System.out.print(f+ " "); 
-	}
-	System.out.print("\n\nMoving all Zeros to the left:\n");
-
-	int end = arr.length - 1;
-
-	for(int i = 0; i <=end;i++){
-	if(arr[i] % 10 == 0){
-	int temp = arr[i];
-	arr[i] = arr[end];
-	arr[end] = temp;
-	end--;
-    i--;
-	}
-	}
-
-    for(int s : arr){
-        System.out.print(s + " ");
+    public static void main(String [] args){
+        int [] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        int [] arr1 = new int[arr.length];
+        int index = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                arr1[index] = arr[i];
+                index++;
+            }
+        }
+        
+        for(int s: arr1){
+            System.out.print(s + " ");
+        }
     }
-}
 }
