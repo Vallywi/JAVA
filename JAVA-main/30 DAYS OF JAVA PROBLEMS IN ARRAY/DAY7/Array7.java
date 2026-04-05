0b93c0f5ab5ac096ac54ef626dbb12d0497d1ff0
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-04 14:35:21 -1200

    Rotate an array to left by one position

--- JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY7/Array7.java
+++ JAVA-main/30 DAYS OF JAVA PROBLEMS IN ARRAY/DAY7/Array7.java
@@ -1,31 +1,31 @@
 package DAY7;
+//8.	Rotate an array to the right by one position
 // [3, 4, 10, 9, 8, 4, 1, 23];
 // [23, 3, 4, 10, 9, 8, 4, 1];
-// Right rotate array by 1 step
+
+class Solution{
+    int [] left(int [] nums, int first){
+        
+        for(int i = nums.length-1; i > 0; i--){
+            nums[i] = nums[i-1];
+        }
+        nums[0] = first;
+        return nums;
+    }
+}
+
 
 public class Array7{
-	public static void main(String [] args){
-	int [] arr = {3, 4, 10, 9, 8, 4, 1, 23};
-    
-    System.out.println("Original Array format: ");
-    
-    for(int s : arr){
-        System.out.print(s + " ");
+    public static void main(String [] args){
+        int [] nums = {3, 4, 10, 9, 8, 4, 1, 23};
+        int first = nums[nums.length-1];
+        
+        Solution sol = new Solution();
+        int [] result = sol.left(nums, first);
+        
+        for(int s : result){
+            System.out.print(s + " ");
+        }
+        
     }
-    
-    System.out.println("\n");
-    int last = arr[arr.length -1];
-    
-	for(int i = arr.length - 1; i > 0; i--){
-	arr[i] = arr[i-1];
-	}
-	
-	System.out.println("Editted: ");
-
-    arr[0] = last;
-	for(int s : arr){
-	System.out.print(s + " ");
-	}
-	
-}
 }
